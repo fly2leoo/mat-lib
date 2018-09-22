ea99bf735241a72fcf28dd9b5d8e887f5caa12e6
Zhiyuan Zou <zouzhyuan@163.com>
2018-09-22 11:31:23 +0800

    init codes

---
+++ git.index.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/fly2leoo/matfigure.git
+git clone https://github.com/fly2leoo/matimg.git
+git clone https://github.com/fly2leoo/matlogger.git
+git clone https://github.com/fly2leoo/matrand.git
+git clone https://github.com/fly2leoo/matsort.git
+git clone https://github.com/fly2leoo/matstring.git
+git clone https://github.com/fly2leoo/mattime.git
+git clone https://github.com/fly2leoo/matutil.git
